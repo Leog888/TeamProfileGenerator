4b672db625e6bce43f60d38478980786488c2e2b
Leo <leonardodi.gutierrez@gmail.com>
2021-10-18 20:43:51 -0700

    new code onto intern.js

--- Intern.js
+++ Intern.js
@@ -1,10 +1,27 @@
-class intern {
+class Intern {
     constructor(name, id, email, school){
-        this.name = name;
-        this.id = id;
-        this.email = email;
-        this.school = school;
+      if (!name, !id, !email,  !school){
+        throw new Error('Please enter all arguments for Intern class!')
+      } else {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+      this.school = school;
+      this.role = 'Intern'
+      }
     }
-}
-
-module.exports = intern;
+    getRole(){
+      return this.role;
+    }
+    getId(){
+      return this.id;
+    }
+    getEmail(){
+      return this.email;
+    }
+    getSchool(){
+      return this.school;
+    }
+  }
+  
+  module.exports = Intern;
